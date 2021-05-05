7f61c8c4cb6d13526b58bed68a163f12a4e325d6
ErciliaR <ercilia_regina@hotmail.com>
2021-05-05 11:56:27 -0300

    validando arredondamento de botão

--- BaladinhaTop/TelasDeEntrada/BemVindoViewController.swift
+++ BaladinhaTop/TelasDeEntrada/BemVindoViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewControllerBemVindo: UIViewController {
+class BemVindoViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
