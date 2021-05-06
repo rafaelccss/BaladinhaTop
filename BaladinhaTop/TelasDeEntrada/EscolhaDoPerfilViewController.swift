5104decac825a5c571af4aa260c433adc03884c5
Francisca Rosa <f.rosa96@hotmail.com>
2021-05-06 19:49:39 -0300

    Tela de escolha do perfil com layout ok. Precisa fazer na main: link para as telas que Erci e Rafa criaram

--- BaladinhaTop/TelasDeEntrada/EscolhaDoPerfilViewController.swift
+++ BaladinhaTop/TelasDeEntrada/EscolhaDoPerfilViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class EscolhaDoPerfilViewController: UIViewController {
 
-    override func viewDidLoad() {
+	@IBOutlet weak var anfitriaoPerfilButton: UIButton!
+	
+	@IBOutlet weak var baladeiroPerfilButton: UIButton!
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
 
+		anfitriaoPerfilButton.backgroundColor = UIColor (named: "roxinhoSucesso")
+		anfitriaoPerfilButton.layer.cornerRadius = 24
+		anfitriaoPerfilButton.setTitleColor(.white, for: .normal) // deu vontade de fazer por código pra aprender :)
+		baladeiroPerfilButton.backgroundColor = UIColor (named: "roxinhoSucesso")
+		baladeiroPerfilButton.layer.cornerRadius = 24
+		baladeiroPerfilButton.setTitleColor(.white, for: .normal)
         // Do any additional setup after loading the view.
     }
     
