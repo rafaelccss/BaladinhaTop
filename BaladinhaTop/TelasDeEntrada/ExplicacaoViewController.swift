73f69c1caa4a771ca37339a84714f670acd847ee
Francisca Rosa <f.rosa96@hotmail.com>
2021-05-06 10:27:22 -0300

    com as imagens e os botões da explicação funcionando Falta arrumar a primeira imagem.

--- BaladinhaTop/TelasDeEntrada/ExplicacaoViewController.swift
+++ BaladinhaTop/TelasDeEntrada/ExplicacaoViewController.swift
@@ -12,24 +12,53 @@ class ExplicacaoViewController: UIViewController {
     @IBOutlet weak var explicacaoLabel: UILabel!
     @IBOutlet weak var barraDeProgresso: UIProgressView!
     @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var explicacaoImage: UIImageView!
+    @IBOutlet weak var voltarExplicacao: UIButton!
+		
+    
+    
     
     var paginaQueEstamos = 1
     
-    @IBAction func proximoButton(_ sender: Any) { //criar image view, trazer pra classe, mudar imagem
-        //fazer autolayout com a mesma lógica do botão, trazer 
-        paginaQueEstamos += 1
-        barraDeProgresso.progress = Float(paginaQueEstamos) / 3
+    @IBAction func voltarButton(_ sender: Any) {
+		if paginaQueEstamos > 1{
+			paginaQueEstamos = paginaQueEstamos - 1
+		} else if paginaQueEstamos > 3 {
+			paginaQueEstamos = 1
+		}
+		
+		oQueExiboNoLabel()
+    }
+   
+    func oQueExiboNoLabel () {
+		barraDeProgresso.progress = Float(paginaQueEstamos) / 3
+		
         switch paginaQueEstamos {
+        
+        case 1:
+            explicacaoLabel.text = "B.Fest é uma proposta para você encontrar a diversão que busca  e que melhor combina com você"
+            explicacaoImage.image = UIImage (named: "seniorImage")
+			voltarExplicacao.isEnabled = false
+			
         case 2:
             explicacaoLabel.text = "Busque por nome, tipo de festa,  estilo de música ou por localidade"
+            explicacaoImage.image = UIImage (named: "seniorLadyImage")
+			voltarExplicacao.isEnabled = true
         case 3:
             explicacaoLabel.text = "Você pode encontrar pessoas para ir com você e traçar a rota perfeita para sua noite"
-        case 4:
-            performSegue(withIdentifier: "toEscolha", sender: nil)
-        default:            
-            print ("oops! q?")
+            explicacaoImage.image = UIImage (named: "seniorsJuntosImage")
+                   
+        default:
+			paginaQueEstamos = 3
+			performSegue(withIdentifier: "toEscolha", sender: nil)
         }
+    }
+    
+    @IBAction func proximoButton(_ sender: Any) {
+        paginaQueEstamos += 1
+       
         
+        oQueExiboNoLabel()
     }
     
 
@@ -37,7 +66,7 @@ class ExplicacaoViewController: UIViewController {
         super.viewDidLoad()
         
         cardView.layer.cornerRadius = 18
-     
+		voltarExplicacao.isEnabled = false
         // Do any additional setup after loading the view.
     }
     
